a4dd1dcbd2768a8cd848bcd7272acbd731f47ce5
백승윤 <seanbryan97@naver.com>
2023-02-21 17:54:03 +0900

    merged index.scss to main.scss. Attempting to draw SVG with svg tag

--- src/index/Index.jsx
+++ src/index/Index.jsx
@@ -1,19 +1,29 @@
 import { Link } from "react-router-dom";
-import "./Index.scss";
 
 function Index() {
   return (
     <div className="index-wrapper">
-      <Link to={"login"}>
-        <div className="login-wrapper">
-          <div className="login"></div>
+      <div className="login-wrapper">
+        <div className="login">
+          <Link to={"login"}>
+            <div className="link">대충 로그인</div>
+          </Link>
         </div>
-      </Link>
-      <Link to={"register"}>
-        <div className="register-wrapper">
-          <div className="register"></div>
+        <svg width={190} height={190} id="login-pointer">
+          <path
+            d="M 10 160  Q 30 110, 90 90 T 180 10"
+            stroke="black"
+            fill="transparent"
+          />
+        </svg>
+      </div>
+      <div className="register-wrapper">
+        <div className="register">
+          <Link to={"register"}>
+            <div className="link">일단 회원가입</div>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
