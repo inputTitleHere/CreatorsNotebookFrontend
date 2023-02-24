b807e457abf33e762bbe1f30848331cd8d9b6a11
백승윤 <seanbryan97@naver.com>
2023-02-24 15:52:41 +0900

    register.jsx임시 저장 -> 자리이동 대비

--- src/index/register/Register.jsx
+++ src/index/register/Register.jsx
@@ -1,9 +1,17 @@
+import { Form } from "react-router-dom";
 import Header from "../../common/Header";
 
 function Register(){
     return(
-        <div className="main">
+        <div>
             <Header showButton={false}/>
+            <section className="register-page">
+                <Form >
+
+                </Form>
+            </section>
         </div>
     )
 }
+
+export default Register;
